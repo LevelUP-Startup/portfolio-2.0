f6eb214b04016ae25dbfa2282d7fb49e65b6aea6
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-05-28 14:30:29 -0300

    feat: scratchs

--- src/pages/Missao/Missao.tsx
+++ src/pages/Missao/Missao.tsx
@@ -6,8 +6,21 @@ import Transition from "../../components/Transition";
 import AboutImg from "../../../public/imgs/levelUpLogo2.png";
 // FRAMER MOTION
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Missao = () => {
+  const [soundClick] = useState<boolean>(false);
+
+  const handleAudioButtonClick = () => {
+    const audio = new Audio("/sounds/button_click.mp3");
+
+    if (soundClick) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+  };
+
   return (
     <Transition onAnimationComplete={() => {}}>
       <section className={[styles.about, styles.container].join(" ")}>
@@ -34,89 +47,6 @@ const Missao = () => {
           >
             <img className={styles.about_img} src={AboutImg} alt="about_img" />
           </motion.div>
-
-          {/* <div className={styles.skills}>
-            <motion.div
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 1,
-                delay: 0.6,
-                ease: [0.2, 0, 0.2, 1],
-              }}
-            >
-              <h3>
-                <span>{`//`}</span> Skills <span>&</span> Tolls
-              </h3>
-            </motion.div>
-
-            <motion.div
-              className={styles.icons_container}
-              variants={container}
-              initial="hidden"
-              animate="visible"
-            >
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>React</span>
-                <BiLogoReact className={styles.react} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Redux</span>
-                <BiLogoRedux className={styles.redux} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Typescript</span>
-                <BiLogoTypescript className={styles.typescript} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Javascript</span>
-                <BiLogoJavascript className={styles.javascript} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>MySQL</span>
-                <GrMysql className={styles.mysql} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Bootstrap</span>
-                <BiLogoBootstrap className={styles.bootstrap} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Git</span>
-                <FaGitAlt className={styles.git} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>CSS</span>
-                <BiLogoCss3 className={styles.css} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>HTML</span>
-                <BiLogoHtml5 className={styles.html} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Firebase</span>
-                <BiLogoFirebase className={styles.firebase} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>Canva</span>
-                <SiCanva className={styles.canva} />
-              </motion.div>
-
-              <motion.div variants={item} className={styles.box_icon}>
-                <span className={styles.icon_descripition}>VScode</span>
-                <BiLogoVisualStudio className={styles.vscode} />
-              </motion.div>
-            </motion.div>
-          </div> */}
         </div>
 
         <div>
@@ -141,7 +71,6 @@ const Missao = () => {
               interativos para garantir que cada jovem atinja potencial/máximo.
             </p>
           </motion.div>
-
           <motion.div
             className={styles.about_content}
             initial={{ opacity: 0, scale: 0 }}
@@ -165,6 +94,19 @@ const Missao = () => {
               práticas e relevantes.
             </p>
           </motion.div>
+
+          <div className={styles.btn_box}>
+            <a
+              href="pdf/riscos-level-up.pdf"
+              download
+              className={styles.btn}
+              onClick={() => {
+                handleAudioButtonClick();
+              }}
+            >
+              Riscos Level UP
+            </a>
+          </div>
         </div>
       </section>
     </Transition>
