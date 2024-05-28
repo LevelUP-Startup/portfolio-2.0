17551d650d32f332fa0bd901f3b347372af26884
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-05-28 14:31:03 -0300

    feat: remove social medias

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,62 +1,62 @@
 // CSS
-import styles from './Contact.module.css'
+import styles from "./Contact.module.css";
 // HOOKS
-import { useState } from 'react'
+import { useState } from "react";
 // REACT ROUTER DOM
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 // COMPONENT
-import Transition from '../../components/Transition'
+import Transition from "../../components/Transition";
 // EMAILJS
-import emailjs from '@emailjs/browser'
+import emailjs from "@emailjs/browser";
 // SWEETALERT
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 // FRAMER MOTION
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 // REACT ICONS
 import {
   FaGithub,
   FaInstagram,
   FaLinkedinIn,
   FaWhatsapp,
-} from 'react-icons/fa6'
+} from "react-icons/fa6";
 
 const Contact = () => {
-  const [name, setName] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
-  const [phone, setPhone] = useState<string>('')
-  const [subject, setSubject] = useState<string>('')
-  const [message, setMessage] = useState<string>('')
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [subject, setSubject] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
-  const [nameError, setNameError] = useState<boolean>(false)
-  const [emailError, setEmailError] = useState<boolean>(false)
-  const [phoneError, setPhoneError] = useState<boolean>(false)
-  const [subjectError, setSubjectError] = useState<boolean>(false)
-  const [messageError, setMessageError] = useState<boolean>(false)
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [phoneError, setPhoneError] = useState<boolean>(false);
+  const [subjectError, setSubjectError] = useState<boolean>(false);
+  const [messageError, setMessageError] = useState<boolean>(false);
 
   function sendEmail(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+    e.preventDefault();
 
     if (
-      name === '' ||
-      email === '' ||
-      phone === '' ||
-      subject === '' ||
-      message === ''
+      name === "" ||
+      email === "" ||
+      phone === "" ||
+      subject === "" ||
+      message === ""
     ) {
-      setNameError(name === '')
-      setEmailError(email === '')
-      setPhoneError(phone === '')
-      setSubjectError(subject === '')
-      setMessageError(message === '')
+      setNameError(name === "");
+      setEmailError(email === "");
+      setPhoneError(phone === "");
+      setSubjectError(subject === "");
+      setMessageError(message === "");
 
-      return
+      return;
     }
 
-    setNameError(false)
-    setEmailError(false)
-    setPhoneError(false)
-    setSubjectError(false)
-    setMessageError(false)
+    setNameError(false);
+    setEmailError(false);
+    setPhoneError(false);
+    setSubjectError(false);
+    setMessageError(false);
 
     const templateParams = {
       name,
@@ -64,41 +64,41 @@ const Contact = () => {
       phone,
       subject,
       message,
-    }
+    };
 
     emailjs
       .send(
-        'service-test',
-        'template_kips',
+        "service-test",
+        "template_kips",
         templateParams,
-        'jepDfkVVycJn4KjTM',
+        "jepDfkVVycJn4KjTM"
       )
       .then(
         (response) => {
           if (response.status === 200) {
             Swal.fire({
-              title: 'Otimo!',
-              text: 'Mensagem enviada com sucesso!',
-              icon: 'success',
-            })
+              title: "Otimo!",
+              text: "Mensagem enviada com sucesso!",
+              icon: "success",
+            });
           }
 
-          console.log('EMAIL ENVIADO', response.status, response.text)
-          setName('')
-          setEmail('')
-          setPhone('')
-          setSubject('')
-          setMessage('')
+          console.log("EMAIL ENVIADO", response.status, response.text);
+          setName("");
+          setEmail("");
+          setPhone("");
+          setSubject("");
+          setMessage("");
         },
         (error) => {
-          console.log('ERRO AO ENVIAR O EMAIL ', error)
-        },
-      )
+          console.log("ERRO AO ENVIAR O EMAIL ", error);
+        }
+      );
   }
 
   return (
     <Transition onAnimationComplete={() => {}}>
-      <section className={[styles.contact, styles.container].join(' ')}>
+      <section className={[styles.contact, styles.container].join(" ")}>
         <video
           className={styles.video_background}
           autoPlay
@@ -122,31 +122,10 @@ const Contact = () => {
               animate={{ opacity: 1, scale: 1 }}
               transition={{
                 duration: 3,
-                delay: 1.2,
-                ease: [0, 0.71, 0.2, 1.01],
-                scale: {
-                  type: 'spring',
-                  damping: 5,
-                  stiffness: 100,
-                  restDelta: 0.001,
-                },
-              }}
-            >
-              <NavLink to="#" className={styles.whatsapp_link}>
-                <FaWhatsapp />
-              </NavLink>
-            </motion.div>
-
-            <motion.div
-              className={styles.icon_container}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 3,
                 delay: 1.5,
                 ease: [0, 0.71, 0.2, 1.01],
                 scale: {
-                  type: 'spring',
+                  type: "spring",
                   damping: 5,
                   stiffness: 100,
                   restDelta: 0.001,
@@ -163,32 +142,11 @@ const Contact = () => {
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
-                duration: 0.3,
-                delay: 1.7,
-                ease: [0, 0.71, 0.2, 1.01],
-                scale: {
-                  type: 'spring',
-                  damping: 5,
-                  stiffness: 100,
-                  restDelta: 0.001,
-                },
-              }}
-            >
-              <NavLink to="#" className={styles.linkedin_link}>
-                <FaLinkedinIn />
-              </NavLink>
-            </motion.div>
-
-            <motion.div
-              className={styles.icon_container}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
                 duration: 3,
                 delay: 1.9,
                 ease: [0, 0.71, 0.2, 1.01],
                 scale: {
-                  type: 'spring',
+                  type: "spring",
                   damping: 5,
                   stiffness: 100,
                   restDelta: 0.001,
@@ -211,18 +169,18 @@ const Contact = () => {
               <input
                 type="text"
                 placeholder="Nome Completo"
-                className={`${styles.item} ${nameError ? styles.error : ''}`}
+                className={`${styles.item} ${nameError ? styles.error : ""}`}
                 id="name"
                 onChange={(e) => {
-                  setName(e.target.value)
-                  setNameError(false)
+                  setName(e.target.value);
+                  setNameError(false);
                 }}
                 value={name}
               />
 
               <div
                 id="error_name"
-                className={`${styles.error_message} ${nameError ? styles.show_message : ''}`}
+                className={`${styles.error_message} ${nameError ? styles.show_message : ""}`}
               >
                 Nome não pode ficar em branco
               </div>
@@ -232,18 +190,18 @@ const Contact = () => {
               <input
                 type="email"
                 placeholder="Endereço do Email"
-                className={`${styles.item} ${emailError ? styles.error : ''}`}
+                className={`${styles.item} ${emailError ? styles.error : ""}`}
                 id="email"
                 onChange={(e) => {
-                  setEmail(e.target.value)
-                  setEmailError(false)
+                  setEmail(e.target.value);
+                  setEmailError(false);
                 }}
                 value={email}
               />
 
               <div
                 id="error_email"
-                className={`${styles.error_message} ${emailError ? styles.show_message : ''}`}
+                className={`${styles.error_message} ${emailError ? styles.show_message : ""}`}
               >
                 Email não pode ficar em branco
               </div>
@@ -255,18 +213,18 @@ const Contact = () => {
               <input
                 type="number"
                 placeholder="Número do Telefone"
-                className={`${styles.item} ${phoneError ? styles.error : ''}`}
+                className={`${styles.item} ${phoneError ? styles.error : ""}`}
                 id="phone"
                 onChange={(e) => {
-                  setPhone(e.target.value)
-                  setPhoneError(false)
+                  setPhone(e.target.value);
+                  setPhoneError(false);
                 }}
                 value={phone}
               />
 
               <div
                 id="error_phone"
-                className={`${styles.error_message} ${phoneError ? styles.show_message : ''}`}
+                className={`${styles.error_message} ${phoneError ? styles.show_message : ""}`}
               >
                 Telefone não pode ficar em branco
               </div>
@@ -277,17 +235,17 @@ const Contact = () => {
                 type="text"
                 placeholder="Assunto"
                 id="subject"
-                className={`${styles.item} ${subjectError ? styles.error : ''}`}
+                className={`${styles.item} ${subjectError ? styles.error : ""}`}
                 onChange={(e) => {
-                  setSubject(e.target.value)
-                  setSubjectError(false)
+                  setSubject(e.target.value);
+                  setSubjectError(false);
                 }}
                 value={subject}
               />
 
               <div
                 id="error_subject"
-                className={`${styles.error_message} ${subjectError ? styles.show_message : ''}`}
+                className={`${styles.error_message} ${subjectError ? styles.show_message : ""}`}
               >
                 Assunto não pode ficar em branco
               </div>
@@ -301,17 +259,17 @@ const Contact = () => {
               id="message"
               cols={30}
               rows={10}
-              className={`${styles.item} ${messageError ? styles.error : ''}`}
+              className={`${styles.item} ${messageError ? styles.error : ""}`}
               onChange={(e) => {
-                setMessage(e.target.value)
-                setMessageError(false)
+                setMessage(e.target.value);
+                setMessageError(false);
               }}
               value={message}
             ></textarea>
 
             <div
               id="error_message"
-              className={`${styles.error_message} ${messageError ? styles.show_message : ''}`}
+              className={`${styles.error_message} ${messageError ? styles.show_message : ""}`}
             >
               Mensagem não pode ficar em branco
             </div>
@@ -325,7 +283,7 @@ const Contact = () => {
         </form>
       </section>
     </Transition>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
